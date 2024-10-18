16ab8da58ced69325431bf2592c9c7a8747c0118
yanghuimin321 <3263656612@qq.com>
2024-10-18 22:21:52 +0800

    feat：添加日夜模式切换动画

---
+++ source/assets/js/sun_moon.js
@@ -0,0 +1,27 @@
+function switchNightMode() {
+  document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"></div></div>'),
+    setTimeout(function() {
+      document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1')),
+        setTimeout(function() {
+          document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
+          document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
+          setTimeout(function() {
+            document.getElementsByClassName('Cuteen_DarkSky')[0].remove();
+          }, 1e3);
+        }, 2e3)
+    })
+  const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
+  if (nowMode === 'light') {
+    btf.activateDarkMode()
+    btf.saveToLocal.set('theme', 'dark', 2)
+    GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+  } else {
+    btf.activateLightMode()
+    btf.saveToLocal.set('theme', 'light', 2)
+    document.querySelector('body').classList.add('DarkMode')
+  }
+  // handle some cases
+  typeof utterancesTheme === 'function' && utterancesTheme()
+  typeof FB === 'object' && window.loadFBComment()
+  window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+}

06f71aea19cdcfd4b38459bf01eecd9f6e4bcb79
yanghuimin321 <3263656612@qq.com>
2024-10-19 20:13:46 +0800

    feat：butterfly魔改樱花特效，菜单毛玻璃，日夜动画优化，页脚透明以及github徽标

--- source/assets/js/sun_moon.js
+++ source/assets/js/sun_moon.js
@@ -1,24 +1,40 @@
 function switchNightMode() {
-  document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"></div></div>'),
-    setTimeout(function() {
-      document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1')),
-        setTimeout(function() {
-          document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
-          document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
-          setTimeout(function() {
-            document.getElementsByClassName('Cuteen_DarkSky')[0].remove();
-          }, 1e3);
-        }, 2e3)
-    })
+  document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"><div id="sun"></div><div id="moon"></div></div></div>'),
+      setTimeout(function () {
+          document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1')),
+              setTimeout(function () {
+                  document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
+                  document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
+                  setTimeout(function () {
+                      document.getElementsByClassName('Cuteen_DarkSky')[0].remove();
+                  }, 1e3);
+              }, 2e3)
+      })
   const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
   if (nowMode === 'light') {
-    btf.activateDarkMode()
-    btf.saveToLocal.set('theme', 'dark', 2)
-    GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+      // 先设置太阳月亮透明度
+      document.getElementById("sun").style.opacity = "1";
+      document.getElementById("moon").style.opacity = "0";
+      setTimeout(function () {
+          document.getElementById("sun").style.opacity = "0";
+          document.getElementById("moon").style.opacity = "1";
+      }, 1000);
+
+      btf.activateDarkMode()
+      btf.saveToLocal.set('theme', 'dark', 2)
+      // GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
   } else {
-    btf.activateLightMode()
-    btf.saveToLocal.set('theme', 'light', 2)
-    document.querySelector('body').classList.add('DarkMode')
+      // 先设置太阳月亮透明度
+      document.getElementById("sun").style.opacity = "0";
+      document.getElementById("moon").style.opacity = "1";
+      setTimeout(function () {
+          document.getElementById("sun").style.opacity = "1";
+          document.getElementById("moon").style.opacity = "0";
+      }, 1000);
+      
+      btf.activateLightMode()
+      btf.saveToLocal.set('theme', 'light', 2)
+      document.querySelector('body').classList.add('DarkMode')
   }
   // handle some cases
   typeof utterancesTheme === 'function' && utterancesTheme()
